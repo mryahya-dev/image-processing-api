bd88df20a22cdd043614a53249cd48971e08788d
yahya-dev <yahyeabdirashid@gmail.com>
2025-11-13 03:51:15 +0500

    ggg

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -6,16 +6,24 @@ import { Component } from '@angular/core';
     <div class="container">
       <header class="header">
         <div class="brand">Image Resizer</div>
-        <nav>
-          <a routerLink="/gallery" routerLinkActive="active">Gallery</a>
-          <a routerLink="/resize" routerLinkActive="active">Resize</a>
-        </nav>
+        <button class="resize-btn" (click)="showResize = !showResize">
+          {{ showResize ? 'Close Resize' : 'Resize Image' }}
+        </button>
       </header>
 
-      <main>
-        <router-outlet></router-outlet>
+      <main class="main-content">
+        <!-- Gallery component always visible -->
+        <app-images-list class="gallery"></app-images-list>
+
+        <!-- Resize form component as a side panel / call-to-action -->
+        <div class="resize-panel" *ngIf="showResize">
+          <app-image-resize></app-image-resize>
+        </div>
       </main>
     </div>
   `,
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  showResize = false;
+}
